af98f0a8fd96c3192b245ef414c90440689cd162
Sara Mohamed <97265093+saraamohameed@users.noreply.github.com>
2022-12-12 01:33:13 +0200

    Add files via upload

---
+++ Bank_Account_Admin.h
@@ -0,0 +1,37 @@
+#ifndef   Bank_Acc_Admin_H_
+#define   Bank_Acc_Admin_H_
+
+#include "STD_Types.h"
+
+typedef struct Account
+{
+    u8 Full_Name[100];
+    u8 Full_Address[200];
+    u8 status[100];
+	u8 Age ;
+	f32 Balance;
+	u8 Guardian_National_ID[15];
+	int Bank_Account_ID;
+	u8 National_ID[15];
+} user_t ;
+
+
+void Create_Account();
+
+void Open_Existing_Account();
+
+void Make_Transaction(int source_id ,int destination_id,double money);
+
+void Get_Cash(int user_id, double money);
+
+void Deposit_in_Account(int user_id, double money);
+
+void Admin_Window();
+
+void Change_Account_Status();
+
+void Client_Window();
+
+
+u64 generatePassword();
+#endif
---
+++ Bank_Account_Client.h
@@ -0,0 +1,14 @@
+
+#ifndef   Bank_Acc_Client_H_
+#define   Bank_Acc_Client_H_
+
+
+
+void Change_Account_Status();
+
+void Client_Window();
+
+void Change_Account_Password();
+
+u64 generatePassword();
+#endif
